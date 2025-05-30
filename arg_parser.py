3b600c669b4f819d2f008522166ec2b26b30601b
9unu <gw.park@khu.ac.kr>
2025-05-30 22:19:33 +0900

    refactor: remove and rename files

--- arg_parser.py
+++
@@ -1,2 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser(description='국내선 항공편 스트리밍 데이터 처리 스크립트')
--- utils/optimize_partition.py
+++
@@ -1,5 +0,0 @@
-def optimize_partitions(df, partition_key, num_partitions=None):
-    """DataFrame 파티션 최적화 및 중복 제거 - count() 호출 없이 효율적으로 구현"""
-    # print('먼저 중복 제거')
-    # df = df.coalesce(8)
-    return df
